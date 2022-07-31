35ea90870bc9ee82a060ee16a6013f9616ba4aa4
jamilee11 <102613055+jamilee11@users.noreply.github.com>
2022-07-30 21:04:22 -0300

    Adicionado Javascript

---
+++ main.js
@@ -0,0 +1,92 @@
+'use strict';
+//A linha use strict instrui 
+//o navegador para usar o modo Estrito, que é como se fosse uma versão mais segura e reduzida do JavaScript.
+
+let banco = [
+    {}
+];
+
+const getBanco = () => JSON.parse(localStorage.getItem ('todoList')) ?? [];
+const setBanco = (banco) => localStorage.setItem ('todoList', JSON.stringify(banco));
+
+const criarItem = (tarefa, status ="", indice) => {
+    const item = document.createElement('label');//para criar a tarefa na lista
+    item.classList.add('todo__item');
+    item.innerHTML = `
+        <input type="checkbox" ${status} data-indice=${indice}>
+        <div>${tarefa}</div>
+        <input type="button" value="X" data-indice=${indice}>
+    `;
+    document.getElementById('todoList').appendChild(item);
+}
+
+const limparTarefas = () => {
+    const todoList = document.getElementById('todoList');
+    while (todoList.firstChild) {
+        todoList.removeChild(todoList.lastChild);
+    }
+}
+
+const atualizarTela = () => {
+    limparTarefas();
+    const banco = getBanco(); 
+    banco.forEach ( (item, indice) => criarItem (item.tarefa, item.status, indice));
+}
+
+const inserirItem = (evento) => {
+    const tecla = evento.key;
+    const texto = evento.target.value;
+    if (tecla === 'Enter'){
+        const banco = getBanco();
+        banco.push ({'tarefa': texto, 'status': ''});
+        setBanco(banco);
+        atualizarTela();
+        evento.target.value = '';
+    }
+}
+
+/*const botaoAdicionar = document.querySelector('button');
+botaoAdicionar.addEventListener('click', function(evento){
+    evento.preventDefault();
+
+    const click = evento.key;
+    const texto = evento.target.value;
+    if(click === 'click'){
+        const banco = getBanco();
+        banco.push({'tarefa': texto, 'status': ''});
+        setBanco(banco);
+        atualizarTela();
+        evento.target.value = '';
+    }
+})*/
+
+const removerItem = (indice) => {
+    const banco = getBanco();
+    banco.splice (indice, 1);//para remover algo do array
+    setBanco(banco);
+    atualizarTela();
+}
+
+const atualizarItem = (indice) => {
+    const banco = getBanco();
+    banco[indice].status = banco[indice].status === '' ? 'checked' : '';
+    setBanco(banco);
+    atualizarTela();
+}
+
+const clickItem = (evento) => {
+    const elemento = evento.target;
+    console.log (elemento.type);
+    if (elemento.type === 'button') {
+        const indice = elemento.dataset.indice;
+        removerItem (indice);
+    }else if (elemento.type === 'checkbox') {
+        const indice = elemento.dataset.indice;
+        atualizarItem (indice);
+    }
+}
+
+document.getElementById('novoItem').addEventListener('keypress', inserirItem);
+document.getElementById('todoList').addEventListener('click', clickItem);
+
+atualizarTela();
